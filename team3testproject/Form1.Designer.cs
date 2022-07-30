2420aa64814f50c0fed27ec901da3c0513565cd8
cwpolitsch <cwpolitsch@mymail.tstc.edu>
2022-07-29 22:54:43 -0500

    Revert "Delete team3testproject directory"
    This reverts commit d5d761468627c5ec23ab5d197a14b0cac9cfd9f6.

---
+++ team3testproject/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace team3testproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogon
+            // 
+            this.btnLogon.Location = new System.Drawing.Point(230, 258);
+            this.btnLogon.Name = "btnLogon";
+            this.btnLogon.Size = new System.Drawing.Size(424, 157);
+            this.btnLogon.TabIndex = 0;
+            this.btnLogon.Text = "Log On";
+            this.btnLogon.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogon;
+    }
+}
+
